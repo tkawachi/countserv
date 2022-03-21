9a72f5d0602ab40c05be01aa03aa5e923d8ee68d
Takashi Kawachi <tkawachi@gmail.com>
2022-03-21 21:58:52 +0900

    membench

---
+++ cmd/membench/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/axiomhq/hyperloglog"
+	"github.com/google/uuid"
+)
+
+func main() {
+	arr := []*hyperloglog.Sketch{}
+	for i := 0; i < 100; i++ {
+		sketch := hyperloglog.New()
+		arr = append(arr, sketch)
+		// Generate random UUID
+		for j := 0; j < 10000; j++ {
+			id := uuid.New()
+			sketch.Insert([]byte(id.String()))
+		}
+	}
+	fmt.Println(len(arr))
+
+	b, err := os.ReadFile("/proc/self/status")
+	if err != nil {
+		panic(err)
+	}
+	// $ go run cmd/membench/main.go | grep -e VmPeak -e VmHWM
+	// VmPeak:  1223084 kB
+	// VmHWM:     13944 kB
+	fmt.Println(string(b))
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/tkawachi/countserv
 
 go 1.17
 
-require github.com/axiomhq/hyperloglog v0.0.0-20220105174342-98591331716a
+require (
+	github.com/axiomhq/hyperloglog v0.0.0-20220105174342-98591331716a
+	github.com/google/uuid v1.3.0
+)
 
 require github.com/dgryski/go-metro v0.0.0-20180109044635-280f6062b5bc // indirect
